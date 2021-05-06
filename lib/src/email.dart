7a6b3583efc2df2b49a046bf83efc9691e3d0c65
Mathieu D'Amours <mathieu@damours.org>
2021-05-06 08:58:26 -0400

    make email matcher more permissive on TLD
    `.health` was not matched at all

--- lib/src/email.dart
+++ lib/src/email.dart
@@ -1,7 +1,7 @@
 import 'package:linkify/linkify.dart';
 
 final _emailRegex = RegExp(
-  r'^(.*?)((mailto:)?[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})',
+  r'^(.*?)((mailto:)?[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z][A-Z]+)',
   caseSensitive: false,
   dotAll: true,
 );
