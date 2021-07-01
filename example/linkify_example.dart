a226177dd4179d394f3b923f2eea6515cb31f6c0
singularity <zhaoxingjian@gmail.com>
2021-07-01 12:28:08 +0800

    Experimental 3

--- example/linkify_example.dart
+++ example/linkify_example.dart
@@ -1,6 +1,8 @@
 import 'package:linkify/linkify.dart';
 
 void main() {
-  print(linkify("Made by https://cretezy.com person@example.com"));
+  print(linkify(
+      "![](https://cdn.jsdelivr.net/gh/fduhole/web@img/2021-06-30/23:14:38.320266.jpg)",
+      options: LinkifyOptions(humanize: false)));
   // Output: [TextElement: 'Made by ', UrlElement: 'https://cretezy.com' (cretezy.com), TextElement: ' ', EmailElement: 'person@example.com' (person@example.com)]
 }
